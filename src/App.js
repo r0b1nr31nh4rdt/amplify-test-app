7cdd5efe1cfab37beff0412546a078466da0eb49
robin <robin.reinhardt@sourcetronic.com>
2023-04-27 08:59:14 +0200

    test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn to code React like a pro
         </a>
       </header>
     </div>
